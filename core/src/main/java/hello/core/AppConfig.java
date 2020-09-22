85ba7ef082b01c7b9721bf17427a7430d3dd90b0
hjs <gufrus@naver.com>
2020-09-23 01:37:32 +0900

    AppConfig 파일 변경

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -8,32 +8,37 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * IoC 컨테이너 또는 DI 컨테이너
  * Application 의 실제 동작에 필요한 구현 객체를 생성한다.
  */
+@Configuration
 public class AppConfig {
 
     /**
      * DI (Dependency Injection) 의존관계 주입 또는 의존성 주입
      * 생성한 객체 인스턴스의 레퍼런스를 생성자를 통해서 주입한다. (30페이지 그림)
-     *
-     *
      */
+    @Bean
     public MemberService memberService() {
         // new MemoryMemberRepository() 객체 인스턴스 생성 후, 참조(레퍼런스) 값을 MemberServiceImpl 생성자의 파라미터로 전달
         return new MemberServiceImpl(memoryMemberRepository());
     }
 
-    private MemoryMemberRepository memoryMemberRepository() {
+    @Bean
+    public MemoryMemberRepository memoryMemberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memoryMemberRepository(), discountPolicy());
     }
 
+    @Bean
     private DiscountPolicy discountPolicy() {
         /**
          * OCP
